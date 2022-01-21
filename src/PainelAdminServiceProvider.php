1e843dd2dfe2682bfece2302be96974bcc2823d3
samuel <samuel@vk2.com.br>
2022-01-21 17:54:29 -0300

    Novos testes

--- src/PainelAdminServiceProvider.php
+++ src/PainelAdminServiceProvider.php
@@ -6,7 +6,7 @@ class PainelAdminServiceProvider extends  \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'Http/controllers' => app_path('Http/Controllers'),
+            __DIR__.'/Http/controllers' => app_path('Http/Controllers'),
         ]);
         $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
         // $this->loadViewsFrom(__DIR__.'/resources/views/Admin/', 'Admin');
