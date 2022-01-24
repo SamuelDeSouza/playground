aad268cb2b3d175b30554910b997ff84f239a8bd
samuel <samuel@vk2.com.br>
2022-01-24 16:36:55 -0300

    novo teste

--- src/PainelAdminServiceProvider.php
+++ src/PainelAdminServiceProvider.php
@@ -1,32 +1,22 @@
 <?php
 // samueldesouza\playground\src\PainelAdminServiceProvider.php
 namespace samueldesouza\playground;
-use Illuminate\Routing\Router;
+// use Illuminate\Routing\Router;
 class PainelAdminServiceProvider extends  \Illuminate\Support\ServiceProvider
 {
-    public function boot(Router $router)
+    public function boot()
     {
         $this->publishes([
             __DIR__.'/Http/controllers' => app_path('Http/Controllers'),
             __DIR__.'/Http/Middleware' => app_path('Http/Middleware'),
             __DIR__.'/Models' => app_path('Models'),
-            __DIR__.'/routes' => base_path('routes'),
+            // __DIR__.'/routes' => base_path('routes'),
             __DIR__.'/resources/views' => resource_path('resources/views'),
             // __DIR__.'/database/migrations' => database_path('/migrations'),
         ]);
-        // $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
         // $this->loadViewsFrom(__DIR__.'/resources/views/Admin/', 'Admin');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        
-        $router->middlewareGroup('admin', array(
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ));
     }
     public function register()
     {
