55df6a8342474f5c9c6143741959100270754b01
samuel <samuel@vk2.com.br>
2022-01-24 08:45:47 -0300

    Novos teste das routes

--- src/PainelAdminServiceProvider.php
+++ src/PainelAdminServiceProvider.php
@@ -7,8 +7,11 @@ class PainelAdminServiceProvider extends  \Illuminate\Support\ServiceProvider
     {
         $this->publishes([
             __DIR__.'/Http/controllers' => app_path('Http/Controllers'),
+            // __DIR__.'/routes' => base_path('routes'),
+            // __DIR__.'/resources/views' => resource_path('resources/views'),
+            // __DIR__.'/database/migrations' => database_path('/migrations'),
         ]);
-        $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
+        // $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
         // $this->loadViewsFrom(__DIR__.'/resources/views/Admin/', 'Admin');
         // $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
