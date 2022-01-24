708d279d4205a118c377cca8cda969d02fd900b1
samuel <samuel@vk2.com.br>
2022-01-24 11:42:02 -0300

    mais updates

--- src/PainelAdminServiceProvider.php
+++ src/PainelAdminServiceProvider.php
@@ -7,12 +7,13 @@ class PainelAdminServiceProvider extends  \Illuminate\Support\ServiceProvider
     {
         $this->publishes([
             __DIR__.'/Http/controllers' => app_path('Http/Controllers'),
+            __DIR__.'/Http/Middleware' => app_path('Http/Middleware'),
             __DIR__.'/Models' => app_path('Models'),
-            // __DIR__.'/routes' => base_path('routes'),
+            __DIR__.'/routes' => base_path('routes'),
             __DIR__.'/resources/views' => resource_path('resources/views'),
             // __DIR__.'/database/migrations' => database_path('/migrations'),
         ]);
-        $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
+        // $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
         // $this->loadViewsFrom(__DIR__.'/resources/views/Admin/', 'Admin');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
